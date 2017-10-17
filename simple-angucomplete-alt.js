80dd13835430eda260a42ad244ac0fcdf2fc3fb8
Rafael Souza <rafaotec@gmail.com>
2017-10-17 12:13:53 -0200

    Implement ng-change and fix some bugs

--- simple-angucomplete-alt.js
+++ simple-angucomplete-alt.js
@@ -1,28 +1,44 @@
-angular.module('app').directive('simpleAngucomplete', function() {
+angular.module('app').directive('simpleAngucomplete', function($compile) {
     return {
         restrict: 'E',  
         scope: {
-          ngModel: '='
+          ngModel: '=',
+          ngChange: '&'
         },      
         templateUrl: 'bower_components/simple-angucomplete-alt/simple-angucomplete-alt.template.html',        
-        replace: true,
-        require: 'ngModel',
+        replace: true,        
+        require: 'ngModel',        
         controller: function($scope, $element, $http, $timeout, $compile) {
-            
-            $scope.$watch('ngModel', function(newValue){                    
-                $scope.getInitialValue();         
+            $scope.lastestNgModel = null;
+
+            $scope.$watch('ngModel', function(newValue){
+                if(newValue!=$scope.lastestNgModel)                 
+                    $scope.getInitialValue();         
             }, true);
 
             $scope.selectedObject = function(selected) {                          
                 if (selected) {
-                    $scope.ngModel = selected.originalObject.id;
+                    $scope.ngModel = selected.originalObject.id;                    
                 }else{
                     $scope.ngModel = null;                    
+                }
+
+                if($scope.lastestNgModel != $scope.ngModel){
+                    $scope.lastestNgModel = angular.copy($scope.ngModel);
+                    if($scope.ngChange){
+                        if($scope.timeoutChange)
+                            timeout.cancel($scope.timeoutChange);
+                            
+                        $scope.timeoutChange = $timeout($scope.ngChange, 300);
+                    }
                 }
             };    
 
             $scope.clearIfNotSelected = function(){
-                $timeout(function(){
+                if($scope.timeoutClear)
+                    $timeout.cancel($scope.timeoutClear);
+
+                $scope.timeoutClear = $timeout(function(){
                     if(!$scope.ngModel)
                         $scope.$broadcast('angucomplete-alt:clearInput', $scope.id);                    
                 }, 300);                
@@ -44,18 +60,22 @@ angular.module('app').directive('simpleAngucomplete', function() {
                 }else{
                     $scope.$broadcast('angucomplete-alt:clearInput', $scope.id);                    
                 }
-            };            
+            };    
         },
-        link: function($scope, element, attrs) {
+        link: function($scope, element, attrs, ctrl) {  
+            ctrl.$viewChangeListeners.push(function() {
+              scope.$eval(attr.ngChange);
+            });          
+
             $scope.placeholder = attrs.placeholder;
             $scope.remoteUrl = attrs.remoteUrl;            
             $scope.titleField = attrs.titleField;
-            $scope.descriptionField = attrs.descriptionField;
+            $scope.descriptionField = attrs.descriptionField;                        
             $scope.minlength = attrs.minlength ? attrs.minlength : 2;
             $scope.id = 'autocomplete_'+attrs.ngModel.substr('.', '_');                         
             angular.element('body').on('change', '#'+$scope.id+'_value',  function(){                
                 $scope.clearIfNotSelected();
-            });            
+            });
         }
     };
 });
